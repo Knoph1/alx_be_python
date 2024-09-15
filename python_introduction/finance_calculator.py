4635d56f86bc81e6dbfaf3c8973181f82f013808
knoph1 <knopholuoch@yahoo.com>
2024-09-15 21:36:13 +0300

    Add finance_calculator.py script for calculating and projecting savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,10 @@
 # finance_calculator.py
 
 # Prompt the user for their monthly income
-monthly_income = float(input("Enter your monthly income:"))
+monthly_income = float(input("Enter your monthly income: "))
 
 # Prompt the user for their total monthly expenses
-total_expenses = float(input("Enter your total monthly expenses:"))
+total_expenses = float(input("Enter your total monthly expenses: "))
 
 # Calculate monthly savings
 monthly_savings = monthly_income - total_expenses
